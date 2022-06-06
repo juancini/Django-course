5fabce9b3000a2f878b1a961238d71560ecd42ae
Juan Mancini <juani.mancini98@gmail.com>
2022-06-06 13:30:30 -0300

    Trying to fix error

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,30 @@
+from urllib import request
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
 
+class StartingPageView(ListView):
+      template_name = "blog/index.html"
+      model = Post
+      ordering = ["-date"]
+      context_object_name = "posts"
+      
+      def get_queryset(self):
+          queryset = super().get_queryset()
+          data = queryset[:3]
+          return data
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-      "posts": latest_posts
-    })
+class AllPostsView(ListView):
+      template_name = "blog/all-posts.html"
+      model = Post
+      ordering = ["-date"]
+      context_object_name = "all_posts"
+      
+class PostDetailView(DetailView):
+      template_name = "blog/post-detail.html"
+      model = Post
+      
 
-
-def posts(request):
-    posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-      "all_posts": posts
-    })
-
-
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-      "post": identified_post
-    })
