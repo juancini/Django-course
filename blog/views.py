5c9632c99877beedc8465d1f7dbc42cf1eba6474
Juan Mancini <juani.mancini98@gmail.com>
2022-06-17 23:53:49 -0300

    added Error pop up

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
 from .forms import CommentForm
@@ -23,12 +26,32 @@ class AllPostsView(ListView):
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-class PostDetailView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class PostDetailView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+        
+    def post(self, request, slug):
+        # generate form for comment, getting data from request.POST
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        
+        if comment_form.is_valid():
+            # commit=False retrives data, but dosnt push to database 
+            comment = comment_form.save(commit=False)
+            comment.post = post # edit comment data
+            comment.save() # finally saving comment with post data added
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
